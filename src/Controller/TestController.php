f25636d2f2f989cf4c888eb9420cb1f8f55925d9
konaperez <konapaerezr@gmail.com>
2018-03-14 10:02:29 -0400

    pruebas de la base de datos

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use App\Entity\Test;
 
 class TestController extends Controller
 {
@@ -12,8 +15,25 @@ class TestController extends Controller
      */
     public function index()
     {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to your action: index(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $test = new Test();
+        $test->setDescription('Ergonomic and stylish!');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($test);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Salvado una nueva prueba con la id '.$test->getId());
+        /*
+        
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
+        */
     }
 }
--- src/Entity/Test.php
+++ src/Entity/Test.php
@@ -22,4 +22,20 @@ class Test
       * @ORM\Column(type="text")
       */
      private $description;
+    
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
 }
